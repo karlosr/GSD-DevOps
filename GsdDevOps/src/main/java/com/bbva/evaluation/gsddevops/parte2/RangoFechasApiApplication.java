60c53bf3e56d434e499b66e10bda644f8f5434d1
karlosr <rorc.dl@gmail.com>
2024-10-10 02:03:55 -0600

    Implementación Ejercicio Parte2 y evidencia de la ejecución de la Parte1 y Parte2; complemento de instrucciones de uso dentro de README

---
+++ GsdDevOps/src/main/java/com/bbva/evaluation/gsddevops/parte2/RangoFechasApiApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bbva.evaluation.gsddevops.parte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author roque
+ */
+@SpringBootApplication
+public class RangoFechasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RangoFechasApiApplication.class, args);
+    }    
+}
